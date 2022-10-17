4083e15c0252d31f296e2607d3eac9e54866ef5f
Mael Jamin <mael.jamin@gmail.com>
2022-10-17 21:36:00 +0200

    init project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<x-guest-layout>
+
+    <button class="btn w-64 rounded-full">Button</button>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                test
+            </div>
+        </div>
+    </div>
+</x-guest-layout>
