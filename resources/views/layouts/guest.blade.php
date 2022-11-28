c594511b8869f385daa58e9d741fa8c6a8684341
Mael JAMIN <mael.jamin@gmail.com>
2022-11-28 21:36:03 +0100

    add favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,7 +15,10 @@
         <meta name="google" content="notranslate">
 
         <!-- Favicon -->
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ setting('site.logo') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 
         <title>{{ setting('site.title') }}</title>
 
