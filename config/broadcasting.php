88c71b34d02e9fb5bdf129a3fdd1d144f5af8252
Mael JAMIN <mael.jamin@gmail.com>
2023-01-26 22:26:38 +0100

    fix pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -31,17 +31,17 @@ return [
     'connections' => [
 
         'pusher' => [
-            'driver' => 'pusher',
+            'driver' => env('BROADCAST_DRIVER', 'pusher'),
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'host' => '127.0.0.1',
-                'port' => '6001',
-                'scheme' => 'http',
-                'encrypted' => true
-                //'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                //'host' => '127.0.0.1',
+                //'port' => '6001',
+                //'scheme' => 'http',
+                //'encrypted' => true;
+                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
